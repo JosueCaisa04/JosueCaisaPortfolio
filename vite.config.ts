768729f63dde5d0f92b8b4d48e94a310362b6bd6
JosueCaisa04 <jeremyjosue.caisachana@ammistudents.education>
2026-07-06 19:08:22 +0200

    chore: set fixed base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.APPLET_ID ? './' : '/JosueCaisaPortfolio/',
+    base: '/JosueCaisaPortfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
