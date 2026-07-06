871fc36422a5f4704675cc6b88e9836ef883a9e2
JosueCaisa04 <jeremyjosue.caisachana@ammistudents.education>
2026-07-06 18:39:09 +0200

    build: update base path logic for deployment
    Adjust the Vite base path configuration to rely on the APPLET_ID environment variable instead of GitHub Actions to improve build flexibility.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_ACTIONS === 'true' ? '/Portfolio_JosueCaisachana/' : './',
+    base: process.env.APPLET_ID ? './' : '/Portfolio_JosueCaisachana/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
